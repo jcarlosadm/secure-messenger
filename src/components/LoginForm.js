76f4db6196bfbb98f5cbd61753b99493a5ad2268
jcarlos <carlosmaster10@gmail.com>
2017-11-10 18:38:56 -0300

    emailChanged action created. reducer AuthReducer created. state auth created. email field added to form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import { emailChanged } from '../actions';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
 class LoginForm extends React.Component {
+  emailChange(text) {
+    this.props.emailChanged(text);
+  }
+
   render() {
     return (
-      <View style={{ paddingTop: 15 }}>
-        <Text>Login Form!</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+            label='Email'
+            placeholder='email@provider.com'
+            onChangeText={this.emailChange.bind(this)}
+            value={this.props.email}
+          />
+        </CardSection>
+
+        <CardSection>
+        </CardSection>
+
+        <Text>
+        </Text>
+
+        <CardSection>
+          <Button>
+            Login
+          </Button>
+        </CardSection>
+      </Card>
     );
   }
 }
 
-export default LoginForm;
+const mapStateToProps = (state) => {
+  const { email } = state.auth;
+
+  return { email };
+};
+
+export default connect(mapStateToProps, { emailChanged })(LoginForm);
