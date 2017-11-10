8320ebb57d19bbf175e2ab05d767fa78400d13dd
jcarlos <carlosmaster10@gmail.com>
2017-11-10 17:59:27 -0300

    common components added

---
+++ src/components/common/card.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+export class Card extends React.Component {
+  render() {
+    return (
+      <View style={styles.containerStyle}>
+        {this.props.children}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10
+  }
+});
---
+++ src/components/common/index.js
@@ -0,0 +1,7 @@
+export * from './button';
+export * from './card_section';
+export * from './card';
+export * from './header';
+export * from './input';
+export * from './spinner';
+export * from './Confirm';
